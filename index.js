718322682921772e717b37d5eaad3c7538625b30
Robert Jackson <robert.w.jackson@me.com>
2015-06-19 13:35:40 -0400

    Make this package a noop for Ember 2.0.0.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+var VersionChecker = require('ember-cli-version-checker');
+
 module.exports = {
-  name: 'ember-get-helper'
+  name: 'ember-get-helper',
+
+  init: function() {
+    var checker = new VersionChecker(this);
+    this._checkerForEmber = checker.for('ember', 'bower');
+  },
+
+  treeFor: function() {
+    if (this._checkerForEmber.lt('2.0.0-beta.1')) {
+      return this._super.treeFor.apply(this, arguments);
+    } else {
+      console.warn('ember-get-helper is not required for Ember 2.0.0 and later (a default `get` helper is included with Ember), please remove from your `package.json`.');
+    }
+  }
 };
