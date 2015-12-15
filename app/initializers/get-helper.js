af54417c8a050133bfcbdaae7681780b22e86b9d
Christopher Thorn <chris.thorn@lytics.io>
2015-12-15 11:41:49 -0800

    remove an unused dependency

--- app/initializers/get-helper.js
+++ app/initializers/get-helper.js
@@ -2,7 +2,6 @@ import Em from 'ember';
 import { registerHelper } from 'ember-get-helper/utils/register-helper';
 
 import getHelper from 'ember-get-helper/helpers/get';
-import getHelperGlimmer from 'ember-get-helper/helpers/get-glimmer';
 
 export function initialize(/* container, application */) {
   // Do not register helpers from Ember 1.13 onwards, starting from 1.13 they
