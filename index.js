ec60bf71218a4c70b0fccf41d43cc252f81090d4
Roman Nuritdinov <ky6uk.kun@gmail.com>
2016-06-21 16:22:53 +0300

    Compatibility to Ember CLI v2.6.0

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ module.exports = {
   name: 'ember-get-helper',
 
   init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+
     var checker = new VersionChecker(this);
     this._checkerForEmber = checker.for('ember', 'bower');
   },
