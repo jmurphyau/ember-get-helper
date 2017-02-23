dd97e4f8af3912885cf5d76f7a8ceb6c003829ad
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-02-23 13:36:29 +0700

    Fixed to support checking for ember-source in Ember 2.11+

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ module.exports = {
     var checker = new VersionChecker(this);
 
     this._super.init && this._super.init.apply(this, arguments);
-    this._checkerForEmber = checker.for('ember', 'bower');
+
+    this._emberVersionChecker = this.for('ember-source', 'npm');
+    if (!this._emberVersionChecker.version) {
+      this._emberVersionChecker = checker.for('ember', 'bower');
+    }
   },
 
   treeFor: function() {
-    if (this._checkerForEmber.lt('2.0.0-beta.1')) {
+    if (this._emberVersionChecker.lt('2.0.0-beta.1')) {
       return this._super.treeFor.apply(this, arguments);
     } else if (this.parent === this.project) {
       if (!hasBeenWarned) {
